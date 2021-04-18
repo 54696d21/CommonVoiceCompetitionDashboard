f1159c1d9436df30987129be2a0f5bd162bb7e2b
54696d21 <tim@timgabriel.de>
2021-04-18 12:40:28 +0200

    fix gitignore and set Stefans and my scores to 0 for testing

--- enrolledContributors.py
+++ enrolledContributors.py
@@ -2,7 +2,7 @@ from Contributor import Contributor
 
 enrolledContributors = [
     Contributor(username="54696d21", clientHash="0272ed18c2dfde45ceb61d5f0dd34fcb8026d55cb6876339dc3762c45bc58699",
-                validatedClipsBeginning=11, recordedClipsBeginning=12),
+                validatedClipsBeginning=119, recordedClipsBeginning=284),
     Contributor(username="Stefan Grotz", clientHash="16e3ec4bc4408cae301120760cef8d4b4aeaf07f0ca3884afdb8d04857f675af",
-                validatedClipsBeginning=11, recordedClipsBeginning=12)
+                validatedClipsBeginning=6214, recordedClipsBeginning=719)
 ]
