38e8300ce4dd11791f45ced27afdbdcf10b16b79
54696d21 <tim@timgabriel.de>
2021-04-30 23:09:37 +0200

    set numbers to zero part2

--- enrolledContributorsDE.py
+++ enrolledContributorsDE.py
@@ -4,7 +4,7 @@ enrolledContributors = [
     Contributor(username="54696d21", clientHash="0272ed18c2dfde45ceb61d5f0dd34fcb8026d55cb6876339dc3762c45bc58699",
                 validatedClipsBeginning=425, recordedClipsBeginning=294),
     Contributor(username="Stefan Grotz", clientHash="16e3ec4bc4408cae301120760cef8d4b4aeaf07f0ca3884afdb8d04857f675af",
-                validatedClipsBeginning=6399, recordedClipsBeginning=719),
+                validatedClipsBeginning=6399, recordedClipsBeginning=745),
     Contributor(username="mkohler", clientHash="28982f1e9e2e76ebfdbb2af6c8380dbd6859a0d1543f2469a985b01189c03151",
                 validatedClipsBeginning=67, recordedClipsBeginning=15),
     Contributor(username="Jomosoto", clientHash="be8422cbfe73d00580dae1aa0ff0702ebb87a7dfcebf68ebd5bfbe6549442c11",
