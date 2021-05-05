3df171798f3137d34b8c892aa0cd0a6d7076f9a9
54696d21 <tim@timgabriel.de>
2021-05-05 18:41:50 +0200

    add user

--- enrolledContributorsDE.py
+++ enrolledContributorsDE.py
@@ -12,11 +12,14 @@ enrolledContributors = [
     Contributor(username="Mi_Ka", clientHash="d2d63bdfa92d5e13b91f7bf92e55deb34b1cb943fb98f354a869f962cdbe9f6d",
                 validatedClipsBeginning=0, recordedClipsBeginning=35),
     Contributor(username="UlrichG", clientHash="", validatedClipsBeginning=0, recordedClipsBeginning=0),
-    Contributor(username="pdaiber", clientHash="",validatedClipsBeginning=0, recordedClipsBeginning=0),
+    Contributor(username="pdaiber", clientHash="287dda1cad6a3ee37af0e3b47d576bd9b177ffc549dca8df6d167a8523885953",
+                validatedClipsBeginning=0, recordedClipsBeginning=0),
     Contributor(username="PFischbeck", clientHash="3dfe84932a8a9cc8ffe8a6b2a38f86239cfe39776bef390255311e1e22f07068",
                 validatedClipsBeginning=329, recordedClipsBeginning=0),
     Contributor(username="OZ", clientHash="50babbbe58a1f6f47268fdcdfc092f1faecdabbb21f3bff3b9fb6b13afc0483c",
                 validatedClipsBeginning=126, recordedClipsBeginning=135),
     Contributor(username="schtiehve", clientHash="875164dfb387623eec2c602404a47edec1aa3db20db9edbc319579725262cd76",
-                validatedClipsBeginning=62, recordedClipsBeginning=25)
+                validatedClipsBeginning=62, recordedClipsBeginning=25),
+    Contributor(username="TeachANies", clientHash="",
+                validatedClipsBeginning=0, recordedClipsBeginning=0)
 ]
