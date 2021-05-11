f78222334e6248f663e5e52cb130d8beaeb1fd3f
54696d21 <tim@timgabriel.de>
2021-05-11 21:14:12 +0200

    add contributors

--- enrolledContributorsDE.py
+++ enrolledContributorsDE.py
@@ -34,6 +34,10 @@ enrolledContributors = [
                      validatedClipsBeginning=0, recordedClipsBeginning=0)
     ,    Contributor(username="reinerh", clientHash="aab64da4960ccf37ed9e59c183c0b13026f2d8fdf4ef6b5bdc8461d3542e9dcb",
                      validatedClipsBeginning=250, recordedClipsBeginning=250)
-        # ,    Contributor(username="", clientHash="",
+    ,    Contributor(username="HerrYoshi", clientHash="e98beb8c2ee7c7362219f1dfbb396d12befa5189e6de86536cf5f253f7fa4e1b",
+                     validatedClipsBeginning=25, recordedClipsBeginning=40)
+    ,    Contributor(username="hanswurst", clientHash="d5cdae8b62bb49a4dc2a803ffd7f6e1ff60301f3854e4166d448ef3e8e6d9254",
+                     validatedClipsBeginning=12, recordedClipsBeginning=5)
+          # ,    Contributor(username="", clientHash="",
     #                  validatedClipsBeginning=0, recordedClipsBeginning=0)
 ]
