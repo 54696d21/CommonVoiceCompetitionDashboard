7f6b2ea5a12c6150a6873a7fe2d226992e3464c4
54696d21 <tim@timgabriel.de>
2021-05-11 21:19:44 +0200

    fix formatting

--- enrolledContributorsDE.py
+++ enrolledContributorsDE.py
@@ -11,7 +11,8 @@ enrolledContributors = [
                 validatedClipsBeginning=1581, recordedClipsBeginning=10),
     Contributor(username="Mi_Ka", clientHash="d2d63bdfa92d5e13b91f7bf92e55deb34b1cb943fb98f354a869f962cdbe9f6d",
                 validatedClipsBeginning=0, recordedClipsBeginning=35),
-    Contributor(username="UlrichG", clientHash="", validatedClipsBeginning=0, recordedClipsBeginning=0),
+    Contributor(username="UlrichG", clientHash="",
+                validatedClipsBeginning=0, recordedClipsBeginning=0),
     Contributor(username="pdaiber", clientHash="287dda1cad6a3ee37af0e3b47d576bd9b177ffc549dca8df6d167a8523885953",
                 validatedClipsBeginning=0, recordedClipsBeginning=0),
     Contributor(username="PFischbeck", clientHash="3dfe84932a8a9cc8ffe8a6b2a38f86239cfe39776bef390255311e1e22f07068",
@@ -23,21 +24,21 @@ enrolledContributors = [
     Contributor(username="TeachANies", clientHash="",
                 validatedClipsBeginning=0, recordedClipsBeginning=0),
     Contributor(username="xuiqzy", clientHash="dc9a8a0d43bfd69fa1889ffbebff7ff6464510d72f4610acc05a09af24d27c04",
-                validatedClipsBeginning=715, recordedClipsBeginning=321)
-    ,    Contributor(username="PsikoBlock", clientHash="09c09f0c6626437439a132e815ab5aa83a63d48b6f7810dfb70e1b4ae66e97ca",
-                     validatedClipsBeginning=89, recordedClipsBeginning=0)
-    ,    Contributor(username="ARiHa", clientHash="8f63f9b539257a32eea8d38d2ded2cd65bd1d87e96996aefbc671a9f83ab10dd",
-                     validatedClipsBeginning=41, recordedClipsBeginning=0)
-    ,    Contributor(username="ruyfel", clientHash="98bbcee9442e9c2f88b686e628406972b3df5c72350228634218d74f3de232d2",
-                     validatedClipsBeginning=20, recordedClipsBeginning=0)
-    ,    Contributor(username="dograts", clientHash="b35da1c3567d5d927bdd87187d12e5ea01624bb9fa2b7984cef09e8e1e41b7f6",
-                     validatedClipsBeginning=0, recordedClipsBeginning=0)
-    ,    Contributor(username="reinerh", clientHash="aab64da4960ccf37ed9e59c183c0b13026f2d8fdf4ef6b5bdc8461d3542e9dcb",
-                     validatedClipsBeginning=250, recordedClipsBeginning=250)
-    ,    Contributor(username="HerrYoshi", clientHash="e98beb8c2ee7c7362219f1dfbb396d12befa5189e6de86536cf5f253f7fa4e1b",
-                     validatedClipsBeginning=25, recordedClipsBeginning=40)
-    ,    Contributor(username="hanswurst", clientHash="d5cdae8b62bb49a4dc2a803ffd7f6e1ff60301f3854e4166d448ef3e8e6d9254",
-                     validatedClipsBeginning=12, recordedClipsBeginning=5)
-          # ,    Contributor(username="", clientHash="",
+                validatedClipsBeginning=715, recordedClipsBeginning=321),
+    Contributor(username="PsikoBlock", clientHash="09c09f0c6626437439a132e815ab5aa83a63d48b6f7810dfb70e1b4ae66e97ca",
+                validatedClipsBeginning=89, recordedClipsBeginning=0),
+    Contributor(username="ARiHa", clientHash="8f63f9b539257a32eea8d38d2ded2cd65bd1d87e96996aefbc671a9f83ab10dd",
+                validatedClipsBeginning=41, recordedClipsBeginning=0),
+    Contributor(username="ruyfel", clientHash="98bbcee9442e9c2f88b686e628406972b3df5c72350228634218d74f3de232d2",
+                validatedClipsBeginning=20, recordedClipsBeginning=0),
+    Contributor(username="dograts", clientHash="b35da1c3567d5d927bdd87187d12e5ea01624bb9fa2b7984cef09e8e1e41b7f6",
+                validatedClipsBeginning=0, recordedClipsBeginning=0),
+    Contributor(username="reinerh", clientHash="aab64da4960ccf37ed9e59c183c0b13026f2d8fdf4ef6b5bdc8461d3542e9dcb",
+                validatedClipsBeginning=250, recordedClipsBeginning=250),
+    Contributor(username="HerrYoshi", clientHash="e98beb8c2ee7c7362219f1dfbb396d12befa5189e6de86536cf5f253f7fa4e1b",
+                validatedClipsBeginning=25, recordedClipsBeginning=40),
+    Contributor(username="hanswurst", clientHash="d5cdae8b62bb49a4dc2a803ffd7f6e1ff60301f3854e4166d448ef3e8e6d9254",
+                validatedClipsBeginning=12, recordedClipsBeginning=5)
+    # ,    Contributor(username="", clientHash="",
     #                  validatedClipsBeginning=0, recordedClipsBeginning=0)
 ]
